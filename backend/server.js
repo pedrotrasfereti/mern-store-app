b04e902b8f7901a0fc33077440ab55b6cd26d062
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-13 00:38:49 -0300

    feat(backend): connect to db on app.listen

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDB } from './config/db';
 
 const app = express();
 
@@ -7,5 +8,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000');
 });
