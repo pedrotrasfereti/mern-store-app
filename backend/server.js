e7c41dc5b484c57a1d73ded75ed8bc193977fa0d
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-13 23:37:36 -0300

    chore: set up eslint and prettier

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { connectDB } from './config/db';
+import connectDB from './config/db';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(5000, () => {
-    connectDB();
-    console.log('Server started at http://localhost:5000');
+  connectDB();
+  console.log('Server started at http://localhost:5000');
 });
