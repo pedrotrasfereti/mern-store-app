814a6ed2806affd3cf009d3261a3cd8a81cd513e
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-15 17:29:01 -0300

    feat(backend): add post /product route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,34 @@
 import express from 'express';
-import connectDB from './config/db';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+// Models
+import Product from './models/product.model.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/product', async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide all fields.',
+    });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+
+    return res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error('Error in Create product: ', error.message);
+    return res.status(500).json({ success: false, message: 'Status Error' });
+  }
 });
 
 app.listen(5000, () => {
