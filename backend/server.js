d50764764bb8c518c1b17d3868811c8487f198d8
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-17 22:01:01 -0300

    feat(backend): add DELETE /api/products/:id route

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/api/products', async (_req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json({ success: true, data: products });
+    return res.status(200).json({ success: true, data: products });
   } catch (err) {
-    res
+    return res
       .status(500)
       .json({ success: false, message: 'Error retrieving products' });
   }
@@ -32,7 +32,7 @@ app.post('/api/products', async (req, res) => {
   if (!product.name || !product.price || !product.image) {
     return res.status(400).json({
       success: false,
-      message: 'Please provide all fields.',
+      message: 'Please provide all fields',
     });
   }
 
@@ -43,8 +43,22 @@ app.post('/api/products', async (req, res) => {
 
     return res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
-    console.error('Error in Create product: ', error.message);
-    return res.status(500).json({ success: false, message: 'Status Error' });
+    return res
+      .status(500)
+      .json({ success: false, message: 'Error creating product' });
+  }
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    return res.status(200).json({ success: true, message: 'Product deleted' });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Error deleting product' });
   }
 });
 
