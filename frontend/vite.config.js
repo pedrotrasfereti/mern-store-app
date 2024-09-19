0b24ad7bd54ff09133e98dc516dab5ebd38fb087
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-19 13:48:46 -0300

    chore(frontend): add server, build and alias settings to Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
