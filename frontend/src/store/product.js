02b00035982cd681a193c87781916f61e7f11cb1
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-20 15:46:24 -0300

    feat(frontend): create product store

---
+++ frontend/src/store/product.js
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+const useProductStore = create((set) => ({
+  products: [],
+  createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.image || !newProduct.price) {
+      return { success: false, message: 'Please fill in all fields.' };
+    }
+
+    const res = await fetch('/api/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProduct),
+    });
+
+    const data = await res.json();
+
+    set((state) => ({ products: { ...state.products, data } }));
+
+    return { success: true, message: 'Product created successfully.' };
+  },
+}));
+
+export default useProductStore;
