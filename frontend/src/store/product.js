7e9ed74c4c4f187bc0bd0f8f605e7b945eafe7ca
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-20 16:52:28 -0300

    feat(frontend): add deleteProduct action to product store

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -2,6 +2,11 @@ import { create } from 'zustand';
 
 const useProductStore = create((set) => ({
   products: [],
+  fetchProducts: async () => {
+    const res = await fetch('/api/products');
+    const data = await res.json();
+    set({ products: data.data });
+  },
   createProduct: async (newProduct) => {
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: 'Please fill in all fields.' };
@@ -21,10 +26,22 @@ const useProductStore = create((set) => ({
 
     return { success: true, message: 'Product created successfully.' };
   },
-  fetchProducts: async () => {
-    const res = await fetch('/api/products');
+  deleteProduct: async (productId) => {
+    const res = await fetch(`/api/products/${productId}`, {
+      method: 'DELETE',
+    });
+
     const data = await res.json();
-    set({ products: data.data });
+
+    if (!data.success) {
+      return { success: false, message: data.message };
+    }
+
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== productId),
+    }));
+
+    return { success: true, message: data.message };
   },
 }));
 
