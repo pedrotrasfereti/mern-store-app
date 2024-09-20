0cc3c4fd972aff4f265a66ba22d41741b249d3f8
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-20 13:41:56 -0300

    feat(frontend): add boilerplate React components for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navbar.jsx';
+import CreatePage from './pages/CreatePage.jsx';
+import HomePage from './pages/HomePage.jsx';
 
 function App() {
-  return <Button>Hello</Button>;
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} minH="100vh">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,75 @@
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from '@chakra-ui/react';
+
+import React, { useState } from 'react';
+
+function CreatePage() {
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    image: '',
+  });
+
+  const handleAddProduct = () => {};
+
+  return (
+    <Container maxW="container.sm">
+      <VStack spacing={0}>
+        <Heading as="h1" size="2xl" textAlign="center" my={4}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w="full"
+          bg={useColorModeValue('white', 'gray.800')}
+          p={6}
+          rounded="lg"
+          shadow="md"
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+
+            <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
+}
+
+export default CreatePage;
