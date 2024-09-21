eaffcc9260660348466b3746dacd703e338d273e
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-20 22:22:30 -0300

    feat(backend): declare server port variable

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // To parse JSON request bodies
 app.use(express.json());
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // Use routes
 app.use('/api/products', productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log('Server started at http://localhost:5000');
 });
